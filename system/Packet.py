7f9b4516ed35affc0b9b6712736a3674aa5b1666
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-03-18 00:58:51 +0100

    dodałem pakiet w postaci listy i sprawdziłem czy działa zgodnie z założeniem (dla pakietu długości 8 bit)

---
+++ system/Packet.py
@@ -0,0 +1,25 @@
+class Packet:
+    def __init__(self, length):
+        self.bitList = []
+        self.length = length
+
+    def addBit(self, bit):
+        if len(self.bitList) >= self.length:
+            return -1
+        else:
+            self.bitList.append(bit)
+
+
+# ################## test ########################################
+x = Packet(8)
+for y in range(111):
+    if x.addBit(1) != -1:
+        pass
+    else:
+        print(x.bitList)
+        # send(x.bitList)
+        x = Packet(8)
+        x.addBit(1)
+
+print(len(x.bitList))
+print(x.bitList)
