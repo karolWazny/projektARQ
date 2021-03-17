523bf6407c540f626e98cf60ee86515957c2b39c
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-03-17 21:37:34 +0100

    zmieniłem zmienną xd

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(funnyName):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Kłaniam się, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Kłaniam się, {funnyName}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
