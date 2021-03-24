a4d8a3051e4cc8c2af874119a9b5426d361064ab
zawadzkanatalia <nataliazawadzka.kontakt@gmail.com>
2021-03-24 14:22:20 +0100

    Nadajnik i test

--- system/Transmitter.py
+++ system/Transmitter.py
@@ -1,32 +1,30 @@
 class Transmitter:
-    def __init__(self, packetSize, word, packet):
-        self.packetSize = packetSize
-        self.word = word
-        self.packet = []   # zauważyłem, że tu chyba powinno byc self.packet = packet; potwierdzcie/zmiencie
+    def __init__(self, packetSize, word,packet):
+        self.packetSize=packetSize
+        self.word=word
+        self.packet=[]
 
-
-def divBitString(word, packet, packetSize):
-    lengthWord = (len(word))
-    counter = 0
-    while (counter + 1) * packetSize < lengthWord:
-        packet.append(word[0 + counter * packetSize:packetSize * (counter + 1)])
-        counter = counter + 1
-    packet.append(word[packetSize * counter:lengthWord])
+def divBitString(word , packet, packetSize):
+    lengthWord=(len(word))
+    counter=0
+    while (counter+1)*packetSize<lengthWord:
+        packet.append(word[0+counter*packetSize:packetSize*(counter+1)])
+        counter=counter+1
+    packet.append(word[packetSize*(counter):lengthWord])
     return packet
 
-
 def addBit(packet):
-    packet2 = []
+    packet2=[]
     for element in packet:
-        counter = 0
+        counter=0;
         for bits in element:
-            if bits == '1':
-                counter = counter + 1
-        if counter % 2 == 1:
-            element = element + '1'
+            if(bits=='1'):
+                counter=counter+1
+        if(counter%2==1):
+            element=element+'1'
         else:
-            element = element + '0'
+            element=element+'0'
         packet2.append(element)
-    packet = packet2
+    packet=packet2
     print(packet)
     return packet
