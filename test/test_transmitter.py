6cb7672f3125a6022aacc1cf932c0b1a943714da
karolWazny <karoljanwazny@gmail.com>
2021-03-24 13:32:15 +0100

    konwencja nazw i importów

---
+++ test/test_transmitter.py
@@ -0,0 +1,18 @@
+import unittest
+from ..system.Transmitter import *
+
+
+class TransmitterTestDiv(unittest.TestCase):
+    def test_divBitString(self):
+        results1 = divBitString("010011100101011", [], 4)
+        self.assertTrue(results1 == ['0100', '1110', '0101', '011'])
+
+
+class TransmitterTestAdd(unittest.TestCase):
+    def test_AddBit(self):
+        results2 = addBit(['0100', '1110', '0101', '011'])
+        self.assertTrue(results2 == ['01001', '11101', '01010', '0110'])
+
+
+if __name__ == '__main__':
+    unittest.main()
