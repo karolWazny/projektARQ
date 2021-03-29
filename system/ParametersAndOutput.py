0c63095ed6f9fd3864425ccc6c4f568ab23368ef
karolWazny <karoljanwazny@gmail.com>
2021-03-29 12:30:19 +0200

    zalążek obsługi plików

--- system/ParametersAndOutput.py
+++ system/ParametersAndOutput.py
@@ -1,3 +1,6 @@
+import json
+
+
 class SimulationParameters:
     def __init__(self):
         self.totalLength = None  # int
@@ -12,3 +15,28 @@ class SimulationOutput:
         self.retransmissions = None
         self.errorsTotal = None
         self.errorsUndetected = None
+
+
+def saveParametersToFile(parameters, fileName):
+    with open(fileName, 'w') as fileObject:
+        json.dump(parameters.__dict__, fileObject)
+
+
+def readParametersFromFile(fileName):
+    with open(fileName, 'r') as fileObject:
+        dictionary = json.load(fileObject)
+        parameters = SimulationParameters()
+        parameters.packetLength = dictionary['packetLength']
+        parameters.totalLength = dictionary['totalLength']
+        parameters.encoding = dictionary['encoding']
+        parameters.noiseModel = dictionary['noiseModel']
+    return parameters
+
+
+if __name__ == '__main__':
+    params = SimulationParameters()
+    params.packetLength = 5
+    filename = 'params.json'
+    saveParametersToFile(params, filename)
+    parameters = readParametersFromFile(filename)
+    print(parameters.packetLength)
