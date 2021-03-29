ccc0c78850c666d4cf9ef9dff483e0ba4a7f1326
karolWazny <karoljanwazny@gmail.com>
2021-03-29 11:08:32 +0200

    podstawowe parametry symulacji

---
+++ system/SimulationOutput.py
@@ -0,0 +1,7 @@
+class SimulationParameters:
+    def __init__(self):
+        self.totalLength = None
+        self.packetLength = None
+        self.noiseModel = None
+        self.encoding = None
+
--- test/test_generator.py
+++ test/test_generator.py
@@ -4,6 +4,7 @@ from ..system.Generator import Generator
 
 
 class GeneratorTest(unittest.TestCase):
-    def test_generating(self):
+    def test_generatingLength(self):
         generator = Generator()
         signal = generator.generate(2)
+        self.assertEqual(len(signal), 2)
