2920de0e470ea5062c71c9116ca4144ee0b30e73
karolWazny <karoljanwazny@gmail.com>
2021-03-29 11:31:04 +0200

    zalążek SimulationOutput

--- system/ParametersAndOutput.py
+++ system/ParametersAndOutput.py
@@ -1,7 +1,14 @@
 class SimulationParameters:
     def __init__(self):
-        self.totalLength = None     #int
-        self.packetLength = None    #int
-        self.noiseModel = None      #słownik, gdzie pierwsza para identyfikuje model, pozostałe to parametry
-        self.encoding = None        #słownik, gdzie pierwsza para identyfikuje model, pozostałe to parametry
+        self.totalLength = None  # int
+        self.packetLength = None  # int
+        self.noiseModel = None  # słownik, gdzie pierwsza para identyfikuje model, pozostałe to parametry
+        self.encoding = None  # słownik, gdzie pierwsza para identyfikuje model, pozostałe to parametry
 
+
+class SimulationOutput:
+    def __init__(self):
+        self.transmissionsTotal = None
+        self.retransmissions = None
+        self.errorsTotal = None
+        self.errorsUndetected = None
