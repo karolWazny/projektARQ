74b5d991e632b0d4e3482e41e988b9a173ad0f8c
zawadzkanatalia <nataliazawadzka.kontakt@gmail.com>
2021-04-05 13:47:17 +0200

    Poprawienie dzielenia na pakiety

--- test/test_transmitter.py
+++ test/test_transmitter.py
@@ -1,16 +1,15 @@
 import unittest
-import Transmitter
-from projekty.system.Transmitter import divBitString
-from projekty.system.Transmitter import addBit
+from ..system.Transmitter import *
 
 class TransmitterTestDiv(unittest.TestCase):
     def test_divBitString(self):
-       results1=divBitString("010011100101011",[], 4)
-       self.assertTrue(results1==['0100', '1110', '0101','011'])
+       results1=Transmitter.divBitString("010011100101011",[], 4)
+       self.assertTrue(results1==['0100', '1110', '0101','0110'])
+
 class TransmitterTestAdd(unittest.TestCase):
     def test_AddBit(self):
-        results2=addBit(['0100', '1110', '0101','011'])
-        self.assertTrue(results2==['01001', '11101', '01010','0110'])
+        results2=Transmitter.addBit(['0100', '1110', '0101','0110'])
+        self.assertTrue(results2==['01001', '11101', '01010','01100'])
 
 if __name__ == '__main__':
     unittest.main()
