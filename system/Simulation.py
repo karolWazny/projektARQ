85dfbb05eb020a990b344cda0eb0228bfee71000
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-04-16 01:36:46 +0200

    symulacja bez zapisu

--- system/Simulation.py
+++ system/Simulation.py
@@ -1,10 +1,10 @@
 class Simulation:
-    def __init__(self, signalLength, packetLength, noiseModel, codingModel, generator, transmitter, distortion, decoder, receiver):
+    def __init__(self, signalLength, packetLength, noiseModel, codingModel, generator, transmitter, channel, decoder, receiver):
         self.signalLength = signalLength
         self.packetLength = packetLength
         self.generator = generator
         self.transmitter = transmitter
-        self.distortion = distortion
+        self.channel = channel
         self.decoder = decoder
         self.receiver = receiver
         self.noiseModel = noiseModel
@@ -14,11 +14,17 @@ class Simulation:
         signal = self.generator.generate(self.signalLength)
         packetList = self.transmitter.divBitString(signal, 1, self.packetLength) #dzielenie sygnału na listę 8-bitowych pakietów
         codedPackets = self.transmitter.addBit(packetList)
-        for x in codedPackets:
-            distortedPacket = self.distortion.distort(x)
-            self.decoder.passFrame(distortedPacket)
+        n = 0
+        while n < len(packetList):
+            distortedPacket = self.channel.distort(codedPackets[n])
+            receivedPacket = self.receiver.decodeData(distortedPacket)
+            self.decoder.passFrame(receivedPacket)
             decodedPacket = self.decoder.decode()
-            receivedData = self.receiver #tu też nie do końca wiem jak to użyć
-            return  #musimy uzgodnić co zwracamy (liczba transmisji, liczba retransmisji, pakiety uszkodzone(?) )
+            if packetList[n] != decodedPacket:
+                pass
+                # simulationLog - zapisz ze retransmisja się odbyła (zwiększ licznik retransmisji)
+            else:
+                n += 1
+        return  #musimy uzgodnić co zwracamy (liczba transmisji, liczba retransmisji, pakiety uszkodzone(?) )
 
 
