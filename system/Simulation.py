0bbc27bc32e779173af36837cdc2e1f612cdaf58
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-04-16 03:44:11 +0200

    symulacja z zapisem

--- system/Simulation.py
+++ system/Simulation.py
@@ -1,3 +1,6 @@
+from .ParametersAndOutput import *
+
+
 class Simulation:
     def __init__(self, signalLength, packetLength, noiseModel, codingModel, generator, transmitter, channel, decoder, receiver):
         self.signalLength = signalLength
@@ -9,22 +12,38 @@ class Simulation:
         self.receiver = receiver
         self.noiseModel = noiseModel
         self.codingModel = codingModel
+        self.simulationLog = SimulationLog()
 
     def simulate(self):
+        parameter = SimulationParameters()
+        parameter.packetLength = self.packetLength
+        parameter.noiseModel = self.noiseModel
+        parameter.totalLength = self.signalLength
+        parameter.encoding = self.codingModel
+
+        output = SimulationOutput()
+        output.transmissionsTotal = 0
+        output.retransmissions = 0
+        output.errorsTotal = 0
+
         signal = self.generator.generate(self.signalLength)
-        packetList = self.transmitter.divBitString(signal, 1, self.packetLength) #dzielenie sygnału na listę 8-bitowych pakietów
+        packetList = self.transmitter.divBitString(signal, [], self.packetLength) #dzielenie sygnału na listę 8-bitowych pakietów
         codedPackets = self.transmitter.addBit(packetList)
         n = 0
         while n < len(packetList):
+            output.transmissionsTotal += 1
             distortedPacket = self.channel.distort(codedPackets[n])
             receivedPacket = self.receiver.decodeData(distortedPacket)
             self.decoder.passFrame(receivedPacket)
             decodedPacket = self.decoder.decode()
             if packetList[n] != decodedPacket:
-                pass
-                # simulationLog - zapisz ze retransmisja się odbyła (zwiększ licznik retransmisji)
+                output.retransmissions += 1  # simulationLog - zapis ze retransmisja się odbyła (zwiększ licznik retransmisji)
+                output.errorsTotal += 1
             else:
                 n += 1
-        return  #musimy uzgodnić co zwracamy (liczba transmisji, liczba retransmisji, pakiety uszkodzone(?) )
+
+        self.simulationLog.params = parameter
+        self.simulationLog.output = output
+        return self.simulationLog
 
 
