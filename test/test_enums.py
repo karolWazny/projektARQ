59751202694a5bf7a761911e91bed7f7a8162645
karolWazny <karoljanwazny@gmail.com>
2021-04-25 22:46:43 +0200

    zaimplementowane porównywanie enumów ze stringiem - testy

---
+++ test/test_enums.py
@@ -0,0 +1,26 @@
+from ..system.Enums import *
+import unittest
+
+
+class EnumTest(unittest.TestCase):
+    def test_enumEqualsEnum(self):
+        parity = Encoding.PARITY
+        hamming = Encoding.HAMMING
+        hamming2 = Encoding.HAMMING
+        self.assertTrue(hamming == hamming2)
+        self.assertFalse(hamming != hamming2)
+        self.assertTrue(hamming != parity)
+        self.assertFalse(hamming == parity)
+
+    def test_enumEqualsString(self):
+        parityEnum = Encoding.PARITY
+        parityString = 'PARITY'
+        hammingString = 'HAMMING'
+        self.assertTrue(parityEnum == parityString)
+        self.assertTrue(parityString == parityEnum)
+        self.assertFalse(parityString != parityEnum)
+        self.assertFalse(parityEnum != parityString)
+        self.assertTrue(parityEnum != hammingString)
+        self.assertTrue(hammingString != parityEnum)
+        self.assertFalse(hammingString == parityEnum)
+        self.assertFalse(parityEnum == hammingString)
