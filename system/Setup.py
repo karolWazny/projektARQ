799bed9c074e02e808144d40847bf48a4366e284
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-04-26 19:35:58 +0200

    rozdzielenie Setup i Frontend oraz Main

--- system/Setup.py
+++ system/Setup.py
@@ -3,6 +3,7 @@ from .Simulation import Simulation
 from .ParametersAndOutput import SimulationParameters, SimulationLog, SimulationOutput
 from .TransmitterReceiverFactory import TransmitterFactory, ReceiverFactory
 from .Channel import ChannelFactoryFactory
+from .Frontend import UserInteraction
 
 
 class Setup:
@@ -11,46 +12,9 @@ class Setup:
 
     def run(self):
         generator = Generator(self.simulationLog.params.totalLength)
-        channel = ChannelFactoryFactory.buildFactory(self.simulationLog.params)
+        channel = ChannelFactoryFactory.buildFactory(self.simulationLog.params.noiseModel)
         transmitter = TransmitterFactory.createTransmitter(self.simulationLog.params.encoding)
         receiver = ReceiverFactory.createReceiver(self.simulationLog.params.encoding)
         self.simulationLog.output = SimulationOutput()
         simulation = Simulation(generator, channel, transmitter, receiver, self.simulationLog)
         return simulation.simulate()
-
-
-class UserInteraction:
-
-    @staticmethod
-    def choiceParameters():
-        simulationLog = SimulationLog()
-        simParams = SimulationParameters()
-        while simParams.packetLength is not None and simParams.totalLength is not None and simParams.encoding is not None and simParams.noiseModel is not None:
-            try:
-                simParams.totalLength = input("Podaj dlugosc ciagu do transmisji: ")
-                if not type(simParams.totalLength) is int:
-                    raise TypeError("Only integers are allowed")
-                if simParams.totalLength < 0:
-                    raise ValueError("Sorry, no numbers below zero")
-                simParams.packetLength = input("Podaj dlugosc wysylanego pakietu")
-                if not type(simParams.packetLength) is int:
-                    raise TypeError("Only integers are allowed")
-                if simParams.packetLength < 0:
-                    raise ValueError("Sorry, no numbers below zero")
-                simParams.noiseModel = input("Podaj rodzaj znieksztalcania")
-                if not simParams.__eq__(simParams.noiseModel):
-                    raise NameError("Only noiseModels from dictionary are allowed")
-                simParams.encoding = input("Podaj rodzaj kodowania")
-                if not simParams.__eq__(simParams.encoding):
-                    raise NameError("Only codingModels from dictionary are allowed")
-            except (TypeError, ValueError, NameError):
-                pass
-        simulationLog.params = simParams
-        setup = Setup(simulationLog)
-        return setup
-
-
-class Main:
-    if __name__ == '__main__':
-        setup = UserInteraction.choiceParameters()
-        simulationLog = setup.run()
