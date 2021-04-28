0808859357884ef3bf5c3ecc790a6b385c1c8e53
karolWazny <karoljanwazny@gmail.com>
2021-04-28 12:57:02 +0200

    możliwość porównania pakietu z listą

--- test/test_encoder.py
+++ test/test_encoder.py
@@ -4,19 +4,28 @@ from ..system.Encoder import *
 
 class ParityEncoderTest(unittest.TestCase):
     def test_encode1(self):
-        results = ParityEncoder.encode(self, [1,0])
-        self.assertTrue(results == [1,0,1])
+        results = ParityEncoder().encode([1, 0])
+        self.assertTrue(results == [1, 0, 1])
+
     def test_encode2(self):
-        results = ParityEncoder.encode(self, [1,0,1,0])
-        self.assertTrue(results == [1,0,1,0,0])
+        results = ParityEncoder().encode([1, 0, 1, 0])
+        self.assertTrue(results == [1, 0, 1, 0, 0])
+
+    def test_encodeDecode(self):
+        initialPacket = Packet.fromList([0, 1, 0, 1, 0, 1, 0])
+        encodedPacket = ParityEncoder().encode(initialPacket)
+
+
 
 class CRCEncoderTest(unittest.TestCase):
     def test_encode1(self):
-        results = CRCEncoder.encode(self,[1,1,0,1,0,0,1,1,1,0,1,1,1,0,0,1,0,1,1,1],[1,0,1,1])
+        results = CRCEncoder.encode(self, [1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1], [1, 0, 1, 1])
         self.assertTrue(results == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+
     def test_encode2(self):
-        results = CRCEncoder.encode(self,[1,1,0,1,0,0,1,1,1,0,1,1,0,0],[1,0,1,1])
+        results = CRCEncoder.encode(self, [1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0], [1, 0, 1, 1])
         self.assertTrue(results == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+
 
 if __name__ == '__main__':
     unittest.main()
