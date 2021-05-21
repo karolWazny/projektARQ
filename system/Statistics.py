9358dc0f571f57a290169934e627beb00ef10959
zawadzkanatalia <nataliazawadzka.kontakt@gmail.com>
2021-05-21 13:00:39 +0200

    Wykres pudełkowy

--- system/Statistics.py
+++ system/Statistics.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from numpy import mean
+import statistics
 import matplotlib.pyplot as plt
 
 
@@ -67,6 +69,62 @@ class Histogram:
         plt.title("L. wszystkich błędów")
         plt.show()
 
+class FiveNumberSummary:
+    def __init__(self, usefulData):
+        self.usefulData = usefulData
+        self.usefulData = usefulData
+        self.minTransmissionsTotal = 0
+        self.minRetransmissions = 0
+        self.minErrorsTotal = 0
+        self.minErrorsUndetected = 0
+        self.over25TransmissionsTotal = 0
+        self.over25Retransmissions = 0
+        self.over25ErrorsTotal = 0
+        self.over25ErrorsUndetected = 0
+        self.medianTransmissionsTotal = 0
+        self.medianRetransmissions = 0
+        self.medianErrorsTotal = 0
+        self.medianErrorsUndetected = 0
+        self.over75TransmissionsTotal = 0
+        self.over75Retransmissions = 0
+        self.over75ErrorsTotal = 0
+        self.over75ErrorsUndetected = 0
+        self.maxTransmissionsTotal = 0
+        self.maxRetransmissions = 0
+        self.maxErrorsTotal = 0
+        self.maxErrorsUndetected = 0
+
+    def showBoxplot(self):
+        df = pd.DataFrame({'TransmissionsTotal' : self.usefulData.transmissionsTotal,
+                                'Retransmissions' : self.usefulData.retransmissions,
+                                'ErrorsTotal' : self.usefulData.errorsTotal,
+                                'ErrorsUndetected' : self.usefulData.errorsUndetected})
+
+        self.minTransmissionsTotal = min(self.usefulData.transmissionsTotal)
+        self.minRetransmissions = min(self.usefulData.retransmissions)
+        self.minErrorsTotal = min(self.usefulData.errorsTotal)
+        self.minErrorsUndetected = min(self.usefulData.errorsUndetected)
+        self.over25TransmissionsTotal = df['TransmissionsTotal'].quantile([0.25])
+        self.over25Retransmissions = df['Retransmissions'].quantile([0.25])
+        self.over25ErrorsTotal = df['ErrorsTotal'].quantile([0.25])
+        self.over25ErrorsUndetected = df['ErrorsUndetected'].quantile([0.25])
+        self.medianTransmissionsTotal = df['TransmissionsTotal'].quantile([0.5])
+        self.medianRetransmissions = df['Retransmissions'].quantile([0.5])
+        self.medianErrorsTotal = df['ErrorsTotal'].quantile([0.5])
+        self.medianErrorsUndetected = df['ErrorsUndetected'].quantile([0.5])
+        self.over75TransmissionsTotal = df['TransmissionsTotal'].quantile([0.75])
+        self.over75Retransmissions = df['Retransmissions'].quantile([0.75])
+        self.over75ErrorsTotal = df['ErrorsTotal'].quantile([0.75])
+        self.over75ErrorsUndetected = df['ErrorsUndetected'].quantile([0.75])
+        self.maxTransmissionsTotal = max(self.usefulData.transmissionsTotal)
+        self.maxRetransmissions = max(self.usefulData.retransmissions)
+        self.maxErrorsTotal = max(self.usefulData.errorsTotal)
+        self.maxErrorsUndetected = max(self.usefulData.errorsUndetected)
+
+
+        plt.title("Wykres pudełkowy")
+        boxplot = df.boxplot(column = ['TransmissionsTotal','Retransmissions','ErrorsTotal','ErrorsUndetected'])
+        plt.show()
 
 # odtąd są testy działania statystyk
 class SimulationLogTest:
@@ -126,3 +184,5 @@ if __name__ == '__main__':
     avg.showGraph()
     hist = Histogram(preparedLists)
     hist.showGraph()
+    boxplot = FiveNumberSummary(preparedLists)
+    boxplot.showBoxplot()
